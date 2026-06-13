fca93d80bb0b329a6d2f1a17c86d715fefb754ca
Ajie Pangestu <paangestu@gmail.com>
2026-06-13 21:11:40 +0700

    refactor: simplify ;d and ;q logic, remove problematic autocmd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,92 +22,79 @@ end, { desc = "Toggle explorer" })
 
 vim.keymap.set("n", ",,", "<Cmd>normal! ;<CR>", { desc = "Repeat f/t forward" })
 
-local function focus_explorer()
-  local explorer_win = nil
+local function get_explorer_win()
   for _, w in ipairs(vim.api.nvim_list_wins()) do
-    local b = vim.api.nvim_win_get_buf(w)
-    local f = vim.bo[b].filetype
-    if f == "snacks_explorer" or f == "neo-tree" then
-      explorer_win = w
-      break
+    local buf = vim.api.nvim_win_get_buf(w)
+    if vim.bo[buf].filetype == "snacks_explorer" then
+      return w
     end
   end
+  return nil
+end
 
-  if not explorer_win then
-    require("snacks").explorer()
-    vim.wait(150)
-    for _, w in ipairs(vim.api.nvim_list_wins()) do
-      local b = vim.api.nvim_win_get_buf(w)
-      local f = vim.bo[b].filetype
-      if f == "snacks_explorer" or f == "neo-tree" then
-        explorer_win = w
-        break
+local function has_real_buffers()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(buf) then
+      local ft = vim.bo[buf].filetype
+      local name = vim.api.nvim_buf_get_name(buf)
+      if ft ~= "snacks_explorer" and name ~= "" then
+        return true
       end
     end
   end
-
-  if explorer_win and vim.api.nvim_win_is_valid(explorer_win) then
-    vim.api.nvim_set_current_win(explorer_win)
-    vim.cmd("only")
-  end
+  return false
 end
 
 vim.keymap.set("n", "<leader>d", function()
   local win = vim.api.nvim_get_current_win()
   local buf = vim.api.nvim_get_current_buf()
-  local is_float = vim.api.nvim_win_get_config(win).relative ~= ""
-
-  if is_float then
-    vim.api.nvim_win_close(win, true)
-    return
-  end
-
   local ft = vim.bo[buf].filetype
-  if ft == "snacks_explorer" or ft == "neo-tree" then
-    vim.cmd("close")
+
+  if ft == "snacks_explorer" then
     return
   end
 
-  local buftype = vim.bo[buf].buftype
-  if buftype == "quickfix" or buftype == "help" then
-    vim.cmd("close")
+  if vim.api.nvim_win_get_config(win).relative ~= "" then
+    vim.api.nvim_win_close(win, true)
     return
   end
 
   vim.cmd("bdelete!")
 
   vim.defer_fn(function()
-    local has_real_buffer = false
-    for _, w in ipairs(vim.api.nvim_list_wins()) do
-      local b = vim.api.nvim_win_get_buf(w)
-      local f = vim.bo[b].filetype
-      local bt = vim.bo[b].buftype
-      if f ~= "snacks_explorer" and f ~= "neo-tree" and bt == "" then
-        local name = vim.api.nvim_buf_get_name(b)
-        if name ~= "" then
-          has_real_buffer = true
-          break
-        end
+    if not has_real_buffers() then
+      local explorer_win = get_explorer_win()
+      if explorer_win then
+        vim.api.nvim_set_current_win(explorer_win)
       end
     end
-
-    if not has_real_buffer then
-      focus_explorer()
-    end
   end, 50)
-end, { desc = "Close current focus" })
+end, { desc = "Close current buffer" })
 
 vim.keymap.set("n", "<leader>q", function()
-  local wins = vim.api.nvim_list_wins()
-  for _, win in ipairs(wins) do
-    local buf = vim.api.nvim_win_get_buf(win)
-    local ft = vim.bo[buf].filetype
-    if ft ~= "snacks_explorer" and ft ~= "neo-tree" then
-      pcall(vim.cmd, "bdelete!")
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(buf) then
+      local ft = vim.bo[buf].filetype
+      if ft ~= "snacks_explorer" then
+        vim.cmd("silent! bdelete!")
+      end
     end
   end
 
   vim.defer_fn(function()
-    focus_explorer()
+    local explorer_win = get_explorer_win()
+    if not explorer_win then
+      require("snacks").explorer()
+      vim.defer_fn(function()
+        explorer_win = get_explorer_win()
+        if explorer_win then
+          vim.api.nvim_set_current_win(explorer_win)
+          vim.cmd("only")
+        end
+      end, 100)
+    else
+      vim.api.nvim_set_current_win(explorer_win)
+      vim.cmd("only")
+    end
   end, 50)
-end, { desc = "Close all except explorer" })
+end, { desc = "Close all buffers, focus explorer" })
