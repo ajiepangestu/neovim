2b105ed9bccf2bb94057f4104c5023d97a658c04
Ajie Pangestu <paangestu@gmail.com>
2026-06-16 10:58:20 +0700

    feat: ;d closes buffer with explorer fallback, ;q closes all buffers

--- lua/config/keymaps/general.lua
+++ lua/config/keymaps/general.lua
@@ -1,20 +1,45 @@
+-- Repeat f/t forward
 vim.keymap.set("n", ",,", "<Cmd>normal! ;<CR>", { desc = "Repeat f/t forward" })
-vim.keymap.set("n", "<leader>d", ":q<CR>", { desc = "Quit" })
-vim.keymap.set("n", "<leader>q", function()
-	local bufs = vim.api.nvim_list_bufs()
-	local to_delete = {}
-	for _, buf in ipairs(bufs) do
-		if vim.api.nvim_buf_is_loaded(buf) and not vim.bo[buf].filetype:match("snacks_explorer") then
-			table.insert(to_delete, buf)
+
+-- Helper: open explorer and focus
+local function open_explorer()
+	require("snacks").explorer()
+	vim.defer_fn(function()
+		for _, win in ipairs(vim.api.nvim_list_wins()) do
+			local buf = vim.api.nvim_win_get_buf(win)
+			if vim.bo[buf].filetype == "snacks_explorer" then
+				vim.api.nvim_set_current_win(win)
+				return
+			end
+		end
+	end, 50)
+end
+
+-- Close current buffer, open explorer if none left
+vim.keymap.set("n", "<leader>d", function()
+	vim.cmd("bdelete")
+	local has_buf = false
+	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted and not vim.bo[buf].filetype:match("snacks_explorer") then
+			has_buf = true
+			break
 		end
 	end
-	for _, buf in ipairs(to_delete) do
-		if vim.api.nvim_buf_is_valid(buf) then
+	if not has_buf then
+		open_explorer()
+	end
+end, { desc = "Close buffer" })
+
+-- Close all buffers, open explorer
+vim.keymap.set("n", "<leader>q", function()
+	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.api.nvim_buf_is_loaded(buf) and not vim.bo[buf].filetype:match("snacks_explorer") then
 			vim.api.nvim_buf_delete(buf, { force = true })
 		end
 	end
-	require("snacks").explorer()
+	open_explorer()
 end, { desc = "Close all buffers" })
+
 vim.keymap.set({ "n", "i", "x", "s" }, "<leader>w", "<cmd>w<cr><esc>", { desc = "Save" })
 vim.keymap.set({ "n", "x" }, "<C-S-f>", function()
 	LazyVim.format({ force = true })
